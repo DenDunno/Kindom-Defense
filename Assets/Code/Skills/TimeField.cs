39d36fcae6c67166e91f3e35c76267aff650c7d2
DenDunno <60221683+DenDunno@users.noreply.github.com>
2022-05-12 17:49:22 +0300

    VFX import.

---
+++ Assets/Code/Skills/TimeField.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class TimeField : MonoBehaviour
+{
+    [SerializeField] private BoxCollider _collider;
+
+    public void Cast()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+    }
+}
